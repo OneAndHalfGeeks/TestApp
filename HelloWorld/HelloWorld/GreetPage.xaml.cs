5790bf92162ae6e86999a2796f540afa2c16efa3
Jean-Philippe Chenot <studio@13webstreet.co.uk>
2019-10-19 23:09:04 +0100

    Initial test app

---
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class GreetPage : ContentPage
+    {
+
+        public GreetPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Click(object sender, EventArgs e)
+        {
+            DisplayAlert("Title", "Hello World", "OK");
+        }
+
+    }
+}
